a938d9e24b0872e9a983ed0712c6b963afbfd8c3
GaganTut <gstut@live.com>
2017-04-06 16:57:17 -1000

    FInished quicksort visual

--- js/visualjs/createVisuals.js
+++ js/visualjs/createVisuals.js
@@ -27,8 +27,8 @@ const createNumEntity = (parent, value, position) => {
   newEntity.id = "plane" + value;
   newEntity.className = "customPlanes";
   newEntity.setAttribute("planeVal", value);
-  newEntity.setAttribute("material", "color: lightblue");
-  newEntity.setAttribute("geometry", `primitive: plane; width: 1; height: ${value}`);
+  newEntity.setAttribute("material", "color: lime");
+  newEntity.setAttribute("geometry", `primitive: plane; width: 0.5; height: ${value}`);
   newEntity.setAttribute("text", `value: ${value}; color: black; align: center; font: dejavu; width: 15`);
   newEntity.setAttribute("position", getPositions(position));
 
---
+++ js/visualjs/quickVisual.js
@@ -0,0 +1,92 @@
+/*jshint esversion: 6*/
+const createQuickButton = ()=> {
+  let newButton = document.createElement("button");
+  newButton.id = "quickBtn";
+  newButton.innerHTML = "Quick Sort";
+  newButton.width = "10%";
+  newButton.style.position = "fixed";
+  newButton.style.top = "110px";
+  newButton.style.left = "45%";
+  newButton.addEventListener("click", () => {
+    let valueArr = document.querySelectorAll(".customPlanes");
+    let nodeArray = [];
+    for (let i = 0; i < valueArr.length; i++) {
+      nodeArray.push(valueArr[i]);
+    }
+    visualQuick(nodeArray);
+  });
+
+  let parent = document.querySelector("#wholeBody");
+  parent.appendChild(newButton);
+
+  return newButton;
+};
+
+const alignNodesBefore = (nodeArray, reference) => {
+  let valueArr = document.querySelectorAll(".customPlanes");
+  for (let z = nodeArray.length - 1; z >= 0; z--) {
+    reference.parentNode.insertBefore(nodeArray[z], reference.nextSibling);
+  }
+  for (let i = 0; i < valueArr.length; i++) {
+    valueArr[i].removeAttribute("animation");
+    valueArr[i].setAttribute("material", "color: lightblue");
+    valueArr[i].setAttribute("geometry", `primitive: plane; width: 1; height: ${Number(valueArr[i].getAttribute("planeVal"))}`);
+    valueArr[i].setAttribute("text", `value: ${Number(valueArr[i].getAttribute("planeVal"))}; color: black; align: center; font: dejavu; width: 15`);
+    valueArr[i].setAttribute("animation", `property: position; dir: normal; dur: 500; loop: false; to: ${getPositions(Array.from(valueArr[i].parentNode.children).indexOf(valueArr[i]) - 6)}`);
+  }
+};
+
+const alignNodesAfter = (nodeArray, reference) => {
+  let valueArr = document.querySelectorAll(".customPlanes");
+  for (let z = 0; z < nodeArray.length; z++) {
+    reference.parentNode.insertBefore(nodeArray[z], reference);
+  }
+  for (let i = 0; i < valueArr.length; i++) {
+    valueArr[i].removeAttribute("animation");
+    valueArr[i].setAttribute("material", "color: lightblue");
+    valueArr[i].setAttribute("geometry", `primitive: plane; width: 1; height: ${Number(valueArr[i].getAttribute("planeVal"))}`);
+    valueArr[i].setAttribute("text", `value: ${Number(valueArr[i].getAttribute("planeVal"))}; color: black; align: center; font: dejavu; width: 15`);
+    valueArr[i].setAttribute("animation", `property: position; dir: normal; dur: 200; loop: false; to: ${getPositions(Array.from(valueArr[i].parentNode.children).indexOf(valueArr[i]) - 6)}`);
+  }
+};
+
+const animMiddle = (node) => {
+  node.removeAttribute("animation");
+  node.setAttribute("animation", `property: rotation; dir: alternate; dur: 500; loop: false; to: 0 180 0`);
+};
+
+const visualQuick = (nodeArray) => {
+  if (nodeArray.length <= 1) {
+    return nodeArray;
+  }
+
+  let leftArr = [];
+  let middleArr = [nodeArray[0]];
+  let rightArr = [];
+
+  for (let j = 1; j < nodeArray.length; j++) {
+    if (Number(nodeArray[j].getAttribute("planeVal")) >= Number(nodeArray[0].getAttribute("planeVal"))) {
+      rightArr.push(nodeArray[j]);
+    } else {
+      leftArr.push(nodeArray[j]);
+    }
+  }
+  animMiddle(middleArr[0]);
+
+  setTimeout(() => {
+    alignNodesBefore(leftArr, middleArr[0]);
+  }, 1000);
+
+  setTimeout(() => {
+    alignNodesAfter(rightArr,middleArr[middleArr.length -1]);
+  }, 1000);
+
+  setTimeout(() => {
+    visualQuick(leftArr);
+  }, 2000);
+  setTimeout(() => {
+    visualQuick(rightArr);
+  }, 2000);
+};
+
+createQuickButton();
